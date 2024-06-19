007e68dd2cbe9dd77b778d3a74622f5204fccb4e
Pedro Henrique <henriquepedro1912@gmail.com>
2024-06-19 14:56:47 -0300

    Alterado a ordem de show Queue list de nome para ID

--- backend/src/services/QueueService/ListQueuesService.ts
+++ backend/src/services/QueueService/ListQueuesService.ts
@@ -1,7 +1,7 @@
 import Queue from "../../models/Queue";
 
 const ListQueuesService = async (): Promise<Queue[]> => {
-  const queues = await Queue.findAll({ order: [["name", "ASC"]] });
+  const queues = await Queue.findAll({ order: [["id", "ASC"]] });
 
   return queues;
 };
